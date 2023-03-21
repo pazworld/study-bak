ca01112d51b2e779db943230953ab730f6661a41
pazworld <paz@moudamepo.net>
2023-03-22 00:36:06 +0900

    add kitty_server2.erl

---
+++ erlang/great_good/chap16/kitty_server2.erl
@@ -0,0 +1,62 @@
+-module(kitty_server2).
+
+-export([start_link/0, order_cat/4, return_cat/2, close_shop/1, test/0]).
+-export([init/1, handle_call/3, handle_cast/2]).
+
+-record(cat, {name, color=green, description}).
+
+%%% Client API
+start_link() -> my_server:start_link(?MODULE, []).
+
+%% Synchronous call
+order_cat(Pid, Name, Color, Description) ->
+    my_server:call(Pid, {order, Name, Color, Description}).
+
+%% This call is asynchronous
+return_cat(Pid, Cat=#cat{}) ->
+    my_server:cast(Pid, {return, Cat}).
+
+%%% Synchronous call
+close_shop(Pid) ->
+    my_server:call(Pid, terminate).
+
+%%% Server functions
+init([]) -> []. %% no treatment of info here!
+
+handle_call({order, Name, Color, Description}, From, Cats) ->
+    if Cats =:= [] ->
+        my_server:reply(From, make_cat(Name, Color, Description)),
+        Cats;
+       Cats =/= [] ->
+        my_server:reply(From, hd(Cats)),
+        tl(Cats)
+    end;
+
+handle_call(terminate, From, Cats) ->
+    my_server:reply(From, ok),
+    terminate(Cats).
+
+handle_cast({return, Cat = #cat{}}, Cats) ->
+    [Cat|Cats].
+
+%%% Private functions
+make_cat(Name, Col, Desc) ->
+    #cat{name=Name, color=Col, description=Desc}.
+
+terminate(Cats) ->
+    [io:format("~p was set free.~n", [C#cat.name]) || C <- Cats],
+    exit(normal).
+
+test() ->
+    Pid = start_link(),
+    Cat1 = order_cat(Pid, carl, brown, "loves to burn bridges"),
+    io:format("order carl: ~p~n", [Cat1]),
+    io:format("return carl~n"),
+    return_cat(Pid, Cat1),
+    Cat2 = order_cat(Pid, jimmy, orange, "cuddly"),
+    io:format("order jimmy: ~p~n", [Cat2]),
+    Cat3 = order_cat(Pid, jimmy, orange, "cuddly"),
+    io:format("order jimmy again: ~p~n", [Cat3]),
+    io:format("return carl~n"),
+    return_cat(Pid, Cat2),
+    close_shop(Pid).
